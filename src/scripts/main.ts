98000b00ba0d5968a223d67bbf8c77f441856473
natemoo-re <ntmoore@knox.edu>
2018-05-28 23:41:56 -0600

    Add content, update styling

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+const images = document.querySelectorAll('img');
+images.forEach(img => {
+    img.addEventListener("error", (e) => {
+        img.classList.add("unloaded");
+    })
+})
